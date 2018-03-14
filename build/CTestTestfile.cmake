38598f868cc8a0de8291361dec1d3015b65bb1c2
Connie <cmzhang96@gmail.com>
2018-03-13 21:17:20 -0400

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/connie/RoboSquad/src
+# Build directory: /home/connie/RoboSquad/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/connie/RoboSquad/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/connie/RoboSquad/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/connie/RoboSquad/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/connie/RoboSquad/src:$ROS_PACKAGE_PATH"
